e95d91467173be80f632aaf577ac0330638e3019
octaviodev <anonymousgamerpack@gmail.com>
2025-05-11 21:52:08 -0300

    si

---
+++ Ejemplo 5 Funciones.php
@@ -0,0 +1,9 @@
+//Function
+//Las funciones permiten recibir párametros y reciclar código sin necesidad de hacer la aplicación más pesada e ineficaz
+<?php
+function saludar($nombre) {
+    return "Hola, $nombre!";
+}
+
+echo saludar("Toto");
+?>
